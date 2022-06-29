510e3e4bb5be7649dcf11d83d339ed33e955f822
Anastasya Myasnikova <nikipov@gmail.com>
2022-06-29 12:29:31 +0300

    extra sprintf using avoided in _uri_parse

--- common/xibridge_uri_parse.h
+++ common/xibridge_uri_parse.h
@@ -11,49 +11,49 @@
 uint32_t xibridge_parse_uri_dev12(const char *uri, xibridge_parsed_uri *parsed_uri)
 {
 #define XI_NET_SCHEMA "xi-net" 
-	if (parsed_uri == NULL) return 1;
-	memset(parsed_uri -> uri_schema, 0, XI_URI_SCHEMA_LEN);
-	memset(parsed_uri -> uri_server_host, 0, XI_URI_HOST_LEN) ;
-	parsed_uri->uri_device_id.reserve = 0;
-	parsed_uri->uri_device_id.id = 0;
-	parsed_uri->uri_device_id.PID = 0;
-	parsed_uri->uri_device_id.VID = 0;
+    if (parsed_uri == NULL) return 1;
+    memset(parsed_uri->uri_schema, 0, XI_URI_SCHEMA_LEN);
+    memset(parsed_uri->uri_server_host, 0, XI_URI_HOST_LEN);
+    parsed_uri->uri_device_id.reserve = 0;
+    parsed_uri->uri_device_id.id = 0;
+    parsed_uri->uri_device_id.PID = 0;
+    parsed_uri->uri_device_id.VID = 0;
     const char *p = uri;
-	p += strspn(p, " \t");
-	const char *end_s = strstr(p, "://");
- 	int l = 0;
-	if (end_s != 0 && (l = ((int)(end_s - p))) < XI_URI_SCHEMA_LEN)
-	{
-		memcpy(parsed_uri -> uri_schema, p, l);
+    p += strspn(p, " \t");
+    const char *end_s = strstr(p, "://");
+    int l = 0;
+    if (end_s != 0 && (l = ((int)(end_s - p))) < XI_URI_SCHEMA_LEN)
+    {
+        memcpy(parsed_uri->uri_schema, p, l);
         if (strcmp(parsed_uri->uri_schema, XI_NET_SCHEMA) == 0)
-		{
-			p = end_s + 3; // length of separators;
-			end_s = strchr(p, '/');
-			l = 0;
-			if (end_s != 0 && (l = ((int)(end_s - p))) < XI_URI_HOST_LEN)
-			{
-			   memcpy(parsed_uri -> uri_server_host, p, l);
-			   p = end_s + 1;  // length of separator;
-               end_s = strchr(p, 0);
-               if (end_s == 0) return 1;
-               char sdev[24 + 1];  // hex-symbols;
-	       memset (sdev, '0', 24);
-	       if (portable_snprintf(sdev, 24 + 1 ,  "%24s", p) > 0)
-               {
-                     unsigned int vid, pid, id, reserved;
-		     if (sscanf(sdev, "%8x%4x%4x%8x", &reserved, &vid, &pid, &id)
-					   == 4)
-		     {
-	                  parsed_uri->uri_device_id.reserve = (uint16_t)reserved;
-			   parsed_uri->uri_device_id.VID = (uint16_t)vid;
-			   parsed_uri->uri_device_id.PID = (uint16_t)pid;
-			   parsed_uri->uri_device_id.id = (uint32_t)id;
-			   return 0;
-		     }
-	       }
-			   
-	              }
-		}
-}
-return 1;
+        {
+            p = end_s + 3; // length of separators;
+            end_s = strchr(p, '/');
+            l = 0;
+            if (end_s != 0 && (l = ((int)(end_s - p))) < XI_URI_HOST_LEN)
+            {
+                memcpy(parsed_uri->uri_server_host, p, l);
+                p = end_s + 1;  // length of separator;
+                end_s = strchr(p, 0);
+                if (end_s == 0) return 1;
+                char sdev[24 + 1];  // hex-symbols;
+                memset(sdev, '0', 24);
+                strcpy(sdev + (24 - strlen(p)), p);
+                {
+                    unsigned int vid, pid, id, reserved;
+                    if (sscanf(sdev, "%8x%4x%4x%8x", &reserved, &vid, &pid, &id)
+                        == 4)
+                    {
+                        parsed_uri->uri_device_id.reserve = (uint16_t)reserved;
+                        parsed_uri->uri_device_id.VID = (uint16_t)vid;
+                        parsed_uri->uri_device_id.PID = (uint16_t)pid;
+                        parsed_uri->uri_device_id.id = (uint32_t)id;
+                        return 0;
+                    }
+                }
+
+            }
+        }
+    }
+    return 1;
 }
