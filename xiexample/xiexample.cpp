e7e8f7db8f112f87e22153ae993c2c26c19c23e3
Anastasya Myasnikova <nikipov@gmail.com>
2022-07-29 12:34:38 +0300

    add xiexample again

---
+++ xiexample/xiexample.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include "../common/defs.h"
+#include "../client/xibridge.h"
+#include <zf_log.h>
+
+// to make log level controlled
+#if defined(BUILD_SHARED_LIBS_XI)
+   ZF_LOG_DEFINE_GLOBAL_OUTPUT_LEVEL;
+#endif
+/*
+* extern functions to be used in test
+*/
+extern bool test_connect_1(const char *ip, uint32_t dev_num);
+extern bool test_connect_2(const char *ip, uint32_t dev_num);
+extern void test_connect_2_threads();
+extern void test_connect_1_threads();
+
+int main(int /*argc*/, char ** /*argv[]*/)
+{
+	zf_log_set_output_level(ZF_LOG_DEBUG);
+	bool ret = true;
+	ZF_LOGD("Starting test_main...");
+
+    std::cout << "Enter ip-address for ximc xinet-server if you intend to test xibridge with it (or type 'N' if not):\n";
+
+    std::string ip_s;
+    uint32_t dev_num;
+    std::cin >> ip_s;
+    if (!ip_s.empty() && ip_s[0] != 'N' && ip_s[0] != 'n' )
+    {
+        std::cout << "Enter device serial number to be used with ximc-server (decimal unsigned number):\n";
+        std::cin >> dev_num;
+        if (!test_connect_1(ip_s.data(), dev_num))
+        {
+			ret = false;
+        }    
+		if (!ret) return 1;
+        test_connect_1_threads();
+    }
+
+    std::cout << "Enter ip-address for urpc xinet-server if you intend to test xibridge with it (or type 'N' if not):\n";
+    std::cin >> ip_s;
+	if (!ip_s.empty() && ip_s[0] != 'N' && ip_s[0] != 'n' )
+    {
+        std::cout << "Enter device serial number to be used with urpc-server  (decimal unsigned number):\n";
+        std::cin >> dev_num;
+
+        if (!test_connect_2(ip_s.data(), dev_num))
+        {
+            ret = false;
+        }    
+		if (!ret) return 1;
+        test_connect_2_threads();
+    }
+    
+    std::cout << "All is done OK. Press some char key, <-| to exit\n";
+    char key;
+    std::cin >> key;
+    return 0;
+}
---
+++ xiexample/xinet_server_test_1.cpp
@@ -0,0 +1,137 @@
+﻿#include <zf_log.h>
+#include <../common/protocols.h>
+#include <../client/xibridge.h>
+#include "ximc-min/ximc_min.h"
+#include "../common/utils.h"
+
+// to run with ximc-xinet-server
+// !!! select the right address every time as tested
+static char  _DEV_IP[256];
+//static const char *const _IP = "172.16.129.251";
+
+
+PACK(
+struct _gets_re
+{
+    char gets[4];
+    status_t status;
+});
+
+typedef struct _gets_re re_gets;
+
+PACK(
+struct _geng_re
+{
+    char gets[4];
+    engine_settings_t settings;
+});
+
+typedef struct _geng_re re_geng;
+
+bool test_connect_1(const char *ip, uint32_t dev_num)
+{
+	
+    sprintf(_DEV_IP, "xi-net://%s/%x", ip, dev_num);
+    char  *pdata; uint32_t count;
+
+    xibridge_enumerate_adapter_devices(ip, "", &pdata, &count);
+    ZF_LOGD("Count of enumerated devices: %u", count);
+    if (count)
+    {
+        const char *p = pdata;
+        for (int i = 0; i < (int)count; i++)
+        {
+            ZF_LOGD("Enumerated device #%d: URI: %s", i + 1, p);
+            p = strchr(p, 0) + 1;
+        }
+    }
+    xibridge_free_enumerate_devices(pdata);
+
+    xibridge_conn_t conn;
+
+    uint32_t err = xibridge_open_device_connection(_DEV_IP, &conn);
+    if (err)
+    {
+        ZF_LOGE("Cannot open device: %s, error: %s", _DEV_IP, xibridge_get_err_expl(err));
+        xibridge_close_device_connection(&conn);
+        return false;
+    }
+
+   
+    re_gets status;
+    uint32_t err_op = xibridge_device_request_response(&conn, (const uint8_t *)"gets", 4, (uint8_t *)&status, sizeof(re_gets));
+    if (err_op)
+    {
+        ZF_LOGE("Cannot execute gets: %s", xibridge_get_err_expl(err));
+        return false;
+    }
+
+    ZF_LOGD("Speed: %d", status.status.CurSpeed);
+
+    re_geng settings;
+    err_op = xibridge_device_request_response(&conn, (const uint8_t *)"geng", 4, (uint8_t *)&settings, sizeof(re_geng));
+    ZF_LOGD("Nom voltage: %u", settings.settings.NomVoltage);
+    xibridge_close_device_connection(&conn);
+    //char  *pdata; uint32_t count;
+
+    xibridge_enumerate_adapter_devices(ip, "", &pdata, &count);
+    ZF_LOGD("Count of enumerated devices: %u", count);
+    if (count)
+    {
+        const char *p = pdata;
+        for (int i = 0; i < (int)count;  i++)
+        {
+            ZF_LOGD("Enumerated device #%d: URI: %s", i+1, p);
+            p = strchr(p, 0) + 1;
+        }
+    }
+    xibridge_free_enumerate_devices(pdata);
+
+	return true;
+}
+
+static void thread_body(int thread_num)
+{
+	
+	ZF_LOGD("Thread %u: openning connection... \n", thread_num);
+    xibridge_conn_t conn;
+	xibridge_open_device_connection(_DEV_IP, &conn);
+	ZF_LOGD("Thread %u: connection opened, conn_id: %u \n", thread_num, conn.conn_id);
+	ZF_LOGD("Thread %u: sending gets... \n", thread_num);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    status_t status;
+    uint32_t err = xibridge_device_request_response(&conn, (const uint8_t *)"gets", 3, (uint8_t *)&status, sizeof(status_t));
+
+	ZF_LOGD("Thread %u: gets return %s\n", thread_num,
+		err == 0 ? "true" : "false");
+	ZF_LOGD("Thread %u: closing connection %u... \n", thread_num, conn.conn_id);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	xibridge_close_device_connection(&conn);
+	ZF_LOGD("Thread %u: Connection %u closed \n", thread_num, conn.conn_id);
+}
+
+void test_connect_1_threads()
+{
+#define TH_NUM 10
+
+	std::thread *pthreads[TH_NUM];
+
+	for (auto i = 0; i < TH_NUM; i++)
+	{
+		pthreads[i] = new std::thread(thread_body, i);
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+
+	for (auto i = 0; i < TH_NUM; i++)
+	{
+		pthreads[i]->join();
+	}
+
+	for (auto i = 0; i < TH_NUM; i++)
+	{
+		delete pthreads[i];
+	}
+    // to finish all threads
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+}
