e6c9b943336def5da4b4efcfdd18691427ffcf6a
Anastasya Myasnikova <nikipov@gmail.com>
2022-12-29 22:51:06 +0300

    protocol3 class modefied, test for server response made, example for server3 corrected

--- example/example.cpp
+++ example/example.cpp
@@ -6,6 +6,9 @@ extern bool xinet_ximc_usage_example(const char *ip, uint32_t dev_num);
 extern bool xinet_urpc_usage_example(const char *ip, uint32_t dev_num);
 extern void xinet_ximc_threads();
 extern void xinet_urpc_threads();
+extern bool xinet_xibridge_usage_example(const char *ip, uint32_t dev_num);
+extern void xinet_xibridge_threads();
+
 
 int main(int /*argc*/, char ** /*argv[]*/)
 {
@@ -40,6 +43,21 @@ int main(int /*argc*/, char ** /*argv[]*/)
         {
             ret = false;
         }    
+        if (!ret) return 1;
+        //xinet_2_threads();  
+    }
+
+    std::cout << "Enter ip-address for xibridge xinet-server if you intend to test xibridge with it (or type 'N' if not):\n";
+    std::cin >> ip_s;
+    if (!ip_s.empty() && ip_s[0] != 'N' && ip_s[0] != 'n')
+    {
+        std::cout << "Enter device serial number to be used with xibridge-server  (decimal unsigned number):\n";
+        std::cin >> dev_num;
+
+        if (!xinet_xibridge_usage_example(ip_s.data(), dev_num))
+        {
+            ret = false;
+        }
         if (!ret) return 1;
         //xinet_2_threads();  
     }
