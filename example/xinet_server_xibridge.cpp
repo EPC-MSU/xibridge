dee09ed3d65fe6bbbec515d2dcffa9c7ae4eded2
Anastasya Myasnikova <nikipov@gmail.com>
2023-01-12 20:00:18 +0300

    crc added in xibridge-server example intended for urpc-device

--- example/xinet_server_xibridge.cpp
+++ example/xinet_server_xibridge.cpp
@@ -15,6 +15,28 @@ static char  _DEV_IP[256];
 
 // urpc cid size
 #define CID_SIZE 4
+#define CRC_SIZE  2
+static uint16_t get_crc(const uint8_t *pbuf, size_t n)
+{
+    uint16_t crc, carry_flag, a;
+    size_t i, j;
+    crc = 0xffff;
+    for (i = 0; i < n; i++)
+    {
+        crc = crc ^ pbuf[i];
+        for (j = 0; j < 8; j++)
+        {
+            a = crc;
+            carry_flag = a & 0x0001;
+            crc = crc >> 1;
+            if (carry_flag == 1)
+            {
+                crc = crc ^ 0xA001;
+            }
+        };
+    }
+    return crc;
+}
 
 bool xinet_xibridge_usage_example(const char * ip, uint32_t dev_num)
 {
@@ -41,10 +63,16 @@ bool xinet_xibridge_usage_example(const char * ip, uint32_t dev_num)
         xibridge_close_device_connection(&conn);
         return false;
     }
-    unsigned char resp[sizeof(urmc_get_identity_information_t) + CID_SIZE];
-    memset(resp, 0, sizeof(urmc_get_identity_information_t) + CID_SIZE);
+    // urpc ginf sending...
+    unsigned char resp[sizeof(urmc_get_identity_information_t) + CID_SIZE + CRC_SIZE];
+    memset(resp, 0, sizeof(urmc_get_identity_information_t) + CID_SIZE + CRC_SIZE);
+
+    uint8_t req_urpc[CID_SIZE + CRC_SIZE];
+    memcpy(req_urpc, "ginf", CID_SIZE);
+    uint16_t crc = get_crc(req_urpc, CID_SIZE);
+    memcpy(req_urpc+CID_SIZE, (void *)&crc, CRC_SIZE);
    
-    uint32_t ginf_err = xibridge_device_request_response(&conn, (const unsigned char *)"ginf", 4, resp, sizeof(urmc_get_identity_information_t) + CID_SIZE);
+    uint32_t ginf_err = xibridge_device_request_response(&conn, req_urpc, CID_SIZE + CRC_SIZE, resp, sizeof(urmc_get_identity_information_t) + CID_SIZE + CRC_SIZE);
     if (ginf_err)
     {
         printf ("Cannot execute ginf: %s\n", xibridge_get_err_expl(ginf_err));
@@ -58,10 +86,15 @@ bool xinet_xibridge_usage_example(const char * ip, uint32_t dev_num)
     printf("Product name: %s\n", (char *)info.ProductName);
     printf("Controller: %s\n", (char *)info.ControllerName);
 
-    unsigned char resp_st[sizeof(urmc_status_impl_t) + CID_SIZE];
-    memset(resp_st, 0, sizeof(urmc_status_impl_t) + CID_SIZE);
+    // urpc gets sending...
+    unsigned char resp_st[sizeof(urmc_status_impl_t)+CID_SIZE + CRC_SIZE];
+    memset(resp_st, 0, sizeof(urmc_status_impl_t)+CID_SIZE + CRC_SIZE);
 
-    uint32_t gets_err = xibridge_device_request_response(&conn, (const unsigned char *)"gets", 4, resp_st, sizeof(urmc_status_impl_t) + CID_SIZE);
+    memcpy(req_urpc, "gets", CID_SIZE);
+    crc = get_crc(req_urpc, CID_SIZE);
+    memcpy(req_urpc + CID_SIZE, (void *)&crc, CRC_SIZE);
+
+    uint32_t gets_err = xibridge_device_request_response(&conn, req_urpc, CID_SIZE + CRC_SIZE, resp_st, sizeof(urmc_status_impl_t) + CID_SIZE + CRC_SIZE);
     if (gets_err)
     {
         printf("Cannot execute gets: %s\n", xibridge_get_err_expl(gets_err));
